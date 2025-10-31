071236822f5e3f06477810d42db36c06cb3718d1
Georg Rohrhofer <georg.rohrhofer@outlook.com>
2025-11-01 00:15:51 +0100

    refactor (editorconfig): add editorconfig and update formatting

--- NodeBackend/libs/dockerlib/include/dockerlib.h
+++ NodeBackend/libs/dockerlib/include/dockerlib.h
@@ -7,30 +7,31 @@
 
 class Docker {
 private:
-    class Impl;
-    std::unique_ptr<Impl> pImpl;
+  class Impl;
+  std::unique_ptr<Impl> pImpl;
 
 public:
-    Docker(const std::string& socket = "/var/run/docker.sock");
-    ~Docker();
+  Docker(const std::string& socket = "/var/run/docker.sock");
+  ~Docker();
 
-    Docker(const Docker&) = delete;
-    Docker& operator=(const Docker&) = delete;
+  Docker(const Docker&) = delete;
+  Docker& operator=(const Docker&) = delete;
 
-    Docker(Docker&&) noexcept;
-    Docker& operator=(Docker&&) noexcept;
+  Docker(Docker&&) noexcept;
+  Docker& operator=(Docker&&) noexcept;
 
-    std::string startContainer(
-        const std::string& image, 
-        const std::string& containerName = "",
-        const std::vector<std::string>& env = {},
-        const std::map<std::string, std::string>& portBindings = {}
-    );
+  std::string startContainer(
+    const std::string& image, 
+    const std::string& containerName = "",
+    const std::vector<std::string>& env = {},
+    const std::map<std::string, std::string>& portBindings = {}
+  );
 
-    void stopContainer(const std::string& containerIdOrName, int timeout = 10);
-    void removeContainer(const std::string& containerIdOrName, bool force = false);
-    std::string getContainerStatus(const std::string& containerIdOrName);
-    std::vector<std::string> listContainers(bool all = false);
+  void pullImage(std::string& image);
+  void stopContainer(const std::string& containerIdOrName, int timeout = 10);
+  void removeContainer(const std::string& containerIdOrName, bool force = false);
+  std::string getContainerStatus(const std::string& containerIdOrName);
+  std::vector<std::string> listContainers(bool all = false);
 };
 
 #endif
