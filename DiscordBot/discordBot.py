f1d298cecc2b770e4020825b720d305db176819c
Natascha Bierbaum <bierbaum.natascha04@gmail.com>
2025-11-08 21:17:42 +0100

    feature(discord bot): Implement direct messages to users

--- DiscordBot/discordBot.py
+++ DiscordBot/discordBot.py
@@ -11,25 +11,51 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
     ready_event.set()
 
-# Channel ID is provided below in for loop, together with message text
-async def send_message(channel_id: int, message: str) -> Optional[discord.Message]:
+# Message to Server Channel
+# Send Message to Server Channel
+async def _send_message_to_channel(channel_id: int, message: str) -> Optional[discord.Message]:
     channel = client.get_channel(channel_id)
     if channel:
         await channel.send(message)
         return 0
     return 1
 
-# run send_message in for loop, for each channel id that is provided
-async def send_to_channels(channel_ids: list[int], message: str):
+# Sends to given Channels
+async def send_to_all_channels(channel_ids: list[int], message: str):
     await ready_event.wait()
-
     for channel_id in channel_ids:
-        result = await send_message(channel_id, message)
+        result = await _send_message_to_channel(channel_id, message)
         if result == 0:
             print(f"Message sent to channel {channel_id}")
         else:
             print(f"Failed to send message to channel {channel_id}")
 
+# Direct Message to User
+# Send Message to User
+async def _send_message_to_direct(user_id: int, message: str) -> Optional[discord.Message]:
+    try:
+        user = await client.fetch_user(user_id) # schauen ob ich await brauche
+        if user:
+            await user.send(message)
+            return 0
+        else: 
+            return 1
+    except discord.Forbidden:
+        return 2
+    except Exception:
+        return 1
+
+# Sends to given Users
+async def send_to_all_directs(user_ids: list[int], message: str):
+    await ready_event.wait()
+    for user_id in user_ids:
+        result = await _send_message_to_direct(user_id, message)
+        if result == 0:
+            print(f"Message sent to user {user_id}")
+        elif result == 2:
+            print(f"Failed to send message to user {user_id}. Privat Messages are deactivated.")
+        else:
+            print(f"Failed to send message to user {user_id}")
+
 def start_bot(token: str):
     client.run(token)
-    
--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -11,17 +11,30 @@ CORS(app)
 def start_api():
     app.run(host="0.0.0.0", port=6969)
 
-@app.post("/message/send")
-def messageSend():
+@app.post("/message/send/channel")
+def messageSendToChannels():
    
     jdata = request.get_json()    
     future = asyncio.run_coroutine_threadsafe(
-        dbot.send_to_channels(jdata["channels"], jdata["messageContent"]), 
+        dbot.send_to_all_channels(jdata["channels"], jdata["messageContent"]),
         dbot.client.loop
     )
     future.result(timeout=10)
     
     return "Success", 200
+
+@app.post("/message/send/direct")
+def messageSendToDirects():
+   
+    jdata = request.get_json()    
+    future = asyncio.run_coroutine_threadsafe(
+        dbot.send_to_all_directs(jdata["directs"], jdata["messageContent"]),
+        dbot.client.loop
+    )
+    future.result(timeout=10)
+    
+    return "Success", 200
+
 
 if __name__ == "__main__":
     flask_thread = Thread(target=start_api)
