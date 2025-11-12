619e961e88b5b66d992b896ff798ec155f29530b
Natascha Bierbaum <bierbaum.natascha04@gmail.com>
2025-11-12 23:00:40 +0100

    feature(discord bot): Add Enum for return types

--- DiscordBot/discordBot.py
+++ DiscordBot/discordBot.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import discord
 import asyncio
+from enum import Enum
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -17,18 +18,20 @@ async def _send_message_to_channel(channel_id: int, message: str) -> Optional[di
     channel = client.get_channel(channel_id)
     if channel:
         await channel.send(message)
-        return 0
-    return 1
+        return Statuscode.SUCCESS
+    return Statuscode.WRONG_CHANNEL_ERROR
 
 # Sends to given Channels
 async def send_to_all_channels(channel_ids: list[int], message: str):
     await ready_event.wait()
     for channel_id in channel_ids:
         result = await _send_message_to_channel(channel_id, message)
-        if result == 0:
+        if result == Statuscode.SUCCESS:
             print(f"Message sent to channel {channel_id}")
+            return Statuscode.SUCCESS
         else:
             print(f"Failed to send message to channel {channel_id}")
+            return Statuscode.WRONG_CHANNEL_ERROR
 
 # Direct Message to User
 # Send Message to User
@@ -37,25 +40,35 @@ async def _send_message_to_direct(user_id: int, message: str) -> Optional[discor
         user = await client.fetch_user(user_id) # schauen ob ich await brauche
         if user:
             await user.send(message)
-            return 0
+            return Statuscode.SUCCESS
         else: 
-            return 1
+            return Statuscode.UNDIFINED_ERROR
     except discord.Forbidden:
-        return 2
+        return Statuscode.USER_NOT_REACHABLE_ERROR
     except Exception:
-        return 1
+        return Statuscode.WRONG_USER_ERROR
 
 # Sends to given Users
 async def send_to_all_directs(user_ids: list[int], message: str):
     await ready_event.wait()
     for user_id in user_ids:
         result = await _send_message_to_direct(user_id, message)
-        if result == 0:
+        if result == Statuscode.SUCCESS:
             print(f"Message sent to user {user_id}")
-        elif result == 2:
+            return Statuscode.SUCCESS
+        elif result == Statuscode.USER_NOT_REACHABLE_ERROR:
             print(f"Failed to send message to user {user_id}. Privat Messages are deactivated.")
+            return Statuscode.USER_NOT_REACHABLE_ERROR
         else:
             print(f"Failed to send message to user {user_id}")
+            return Statuscode.WRONG_USER_ERROR
+
+class Statuscode(Enum):
+    SUCCESS = 0
+    UNDIFINED_ERROR = 1
+    WRONG_CHANNEL_ERROR = 2
+    WRONG_USER_ERROR = 3
+    USER_NOT_REACHABLE_ERROR = 4
 
 def start_bot(token: str):
     client.run(token)
