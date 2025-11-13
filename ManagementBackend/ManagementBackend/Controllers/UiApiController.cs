f0bbb850aa2e96060b2aee9fa982f0b5bb2d197e
DomDom737 <ortner.do@gmail.com>
2025-11-13 02:37:52 +0100

    Added UiApi Controller

---
+++ ManagementBackend/ManagementBackend/Controllers/UiApiController.cs
@@ -0,0 +1,18 @@
+﻿using ManagementBackend.resources;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementBackend.Controllers
+{
+    [ApiController]
+    [Route("UiApi")]
+    [Authorize]
+    public class UiApiController : ControllerBase
+    {
+        [HttpGet("GetUiData")]
+        public string TestEndpoint()
+        {
+            return "Here is your dummy Ui Data";
+        }
+    }
+}
