dd476e0d26034d9245e9069d2740a874d42a2045
Rohrhofer Georg <206855@fhwn.ac.at>
2025-11-14 10:22:30 +0100

    fix(sockets NodeBackend): remove unused code

--- NodeBackend/src/main.cpp
+++ NodeBackend/src/main.cpp
@@ -12,21 +12,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  // cout << "Hello World" << endl;
-  // Docker d = {};
-  //
-  // auto res = d.listContainers();
-  //
-  // for (auto s : res) {
-  //   cout << s << endl;
-  // }
-  //
-  // string containerName =
-  //     d.startContainer("marctv/minecraft-papermc-server:1.21.10-91");
-  // cout << containerName << endl;
-  // this_thread::sleep_for(chrono::milliseconds(3000));
-  // d.stopContainer(containerName);
-
   QCoreApplication app(argc, argv);
   QEventLoop loop;
 
