51dc73eaad3bc977c6b2dd14ccded1d4ea93001a
Natascha Bierbaum <bierbaum.natascha04@gmail.com>
2025-11-17 16:49:38 +0100

    feature(monitoring service): add library for shared data format

---
+++ MonitoringService/SharedLibraries/SharedLibraries/MonitoringMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedLibraries
+{
+    public class MonitoringMessage
+    {
+        public string MemoryUsage { get; set; } = string.Empty;
+        public string CpuUsage { get; set; } = string.Empty;
+        public string DiskUsage { get; set; } = string.Empty;
+        public string NetworkUsage { get; set; } = string.Empty;
+    }
+}
